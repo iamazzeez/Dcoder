833987d1ba9b70051bed39efccac76826e6301ec
Abdul Azeez <abdaseez@gmail.com>
2019-06-27 19:48:30 +0530

    login or register page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router,  NavLink, Switch, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Login_Register from "./pages/login_register";
+
+
+class App extends Component {
+  state = {
+    loggedIn:false
+  }
+  loginHandle = () => {
+    this.setState(prevState => ({
+     loggedIn: !prevState.loggedIn  
+    }))
+  }
+  render() {
+    return (
+      <Router>
+   <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+          <a className="navbar-brand">Dcoder</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="true" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+      
+  
+      <Switch>
+        <Route path="/" exact strict component={Login_Register} />
+
+        </Switch>
+  </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
