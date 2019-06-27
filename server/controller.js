03c56b48f67b66fb2657f0cdea8371d4ff84b4f9
Abdul Azeez <abdaseez@gmail.com>
2019-06-27 22:49:17 +0530

    db and server created

---
+++ server/controller.js
@@ -0,0 +1,80 @@
+
+const jwt = require('jsonwebtoken');
+const UserModel = require('./schema/user');
+const Threads = require('./schema/threads');
+
+module.exports = function(app) {
+ 
+//Create thread
+app.post('/api/threads', (req,res, next) => {
+  const user = new Threads({
+      email: req.body.email,
+      password: req.body.password
+  })
+   user.save()
+   .then(result =>  res.send(result))
+   .catch(err => res.send(err))
+      
+})
+  
+  app.get('/api', (req, res) => {
+    res.json({
+      message: 'Welcome to the API'
+    });
+  });
+  
+  app.post('/api/posts', verifyToken, (req, res) => {  
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+      if(err) {
+        res.sendStatus(403);
+      } else {
+        res.json({
+          message: 'Post created...',
+          authData
+        });
+      }
+    });
+  });
+  
+  app.post('/api/login', (req, res) => {
+    // Mock user
+    const user = {
+      id: 1, 
+      username: 'azzeez',
+      email: 'azzz@gmail.com'
+    }
+  
+    jwt.sign({user}, 'secretkey', { expiresIn: '30s' }, (err, token) => {
+      res.json({
+        token
+      });
+    });
+  });
+  
+  // FORMAT OF TOKEN
+  // Authorization: Bearer <access_token>
+  
+  // Verify Token
+  function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+      // Split at the space
+      const bearer = bearerHeader.split(' ');
+      // Get token from array
+      const bearerToken = bearer[1];
+      // Set the token
+      req.token = bearerToken;
+      // Next middleware
+      next();
+    } else {
+      // Forbidden
+      res.sendStatus(403);
+    }
+  
+  }
+  
+
+};
+
---
+++ server/schema/threads.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+const Threads = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: Array
+  },
+  username: {
+    type: ObjectId,
+    ref: 'User'
+},
+  date: {
+    type: Date,
+    // `Date.now()` returns the current unix timestamp as a number
+    default: Date.now
+  }
+
+});
+
+
+module.exports = mongoose.model("Threads", Threads);
